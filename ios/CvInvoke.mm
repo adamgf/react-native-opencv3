9648507eaf887f0313b0622b5015f84655a1cac1
Adam Freeman <adamgf@gmail.com>
2019-03-04 15:56:28 -0800

    method invocation now working in C++

--- ios/CvInvoke.mm
+++ ios/CvInvoke.mm
@@ -8,33 +8,76 @@
 //
 #import "CvInvoke.h"
 #import "MatManager.h"
+#include <variant>
 
-std::vector<std::string> lookup = {
+typedef std::variant<int,double,float,const char*,Mat,Scalar,cv::Point> ocvtypes;
+
+std::vector<std::string> Imgproc = {
     "cvtColor"
+};
+
+std::vector<std::string> iptypes = {
+    "Mat,Mat,int,int"
 };
 
 typedef enum fns {
     CVTCOLOR
 } fns;
 
-template<typename ...ArgTypes>
-void callOpenCvMethod(ArgTypes *...p) {
-    
-    std::string functionName("cvtColor");
+template<typename... T>
+void invokeIt(std::vector<std::string>lookup, std::string functionName, T *...args) {
     auto it = std::find(lookup.begin(), lookup.end(), functionName);
     if (it != lookup.end()) {
         auto index = std::distance(lookup.begin(), it);
         
         switch(index) {
-                case CVTCOLOR: {
-                    cvtColor(*p...);
-                    break;
-                }
+            case CVTCOLOR: {
+                cvtColor(*args...);
+                break;
+            }
             default:
                 break;
         }
     }
 }
+
+void callOpenCvMethod(std::string searchClass, std::string functionName, std::vector<ocvtypes*> objects) {
+
+    std::vector<std::string> lookup;
+    NSString *searchStr = [NSString stringWithUTF8String:(searchClass.c_str())];
+    if ([searchStr isEqualToString:@"Imgproc"]) {
+        lookup = Imgproc;
+    }
+    
+    unsigned long numParams = 4; //paramTypes.size();
+    
+    if (numParams == 4) {
+        Mat *firstMat = (Mat*)objects.at(0);
+        auto p1 = *reinterpret_cast<Mat*>(firstMat);
+        Mat *secondMat = (Mat*)objects.at(1);
+        auto p2 = *reinterpret_cast<Mat*>(secondMat);
+        int *firstInt = (int*)objects.at(2);
+        auto p3 = *reinterpret_cast<int*>(firstInt);
+        int *secondInt = (int*)objects.at(3);
+        auto p4 = *reinterpret_cast<int*>(secondInt);
+        invokeIt(lookup, functionName, &p1, &p2, &p3, &p4);
+        int kk = 2002;
+        kk++;
+    }
+}
+
+/**
+void paramsToFunctionCall(std::string fname, std::vector<void*> args, std::string sclass) {
+    
+    unsigned long argCount = args.size();
+    
+    if (argCount == 0) {
+        callOpenCvMethod(fname, NULL, sclass);
+    }
+    else if (argCount == 1) {
+        
+    }
+} */
 
 @interface MatWrapper2 : NSObject
 @property (nonatomic, assign) Mat myMat;
@@ -75,6 +118,7 @@ void callOpenCvMethod(ArgTypes *...p) {
         NSString* param = NSStringFromClass(paramClass);
         NSString* itsType = NSStringFromClass([hashMap valueForKey:paramNum]);
         if ([itsType isEqualToString:@"NSString"]) {
+            // special case for a Mat being represented by a hardcoded string representation ...
             NSString *paramStr = [hashMap valueForKey:paramNum];
             Mat dstMat;
             if ([paramStr isEqualToString:@"rgba"]) {
@@ -100,12 +144,12 @@ void callOpenCvMethod(ArgTypes *...p) {
                 [retObjs insertObject:MW2 atIndex:(i-1)];
                 
             }
-            else if ([param isEqualToString:@"NSString"]) {
+            else if ([param isEqualToString:@"const char*"]) {
                 [retObjs insertObject:paramStr atIndex:(i-1)];
             }
         }
         else if ([param isEqualToString:@"Mat"] || [param isEqualToString:@"InputArray"] || [param isEqualToString:@"InputArrayOfArrays"] || [param isEqualToString:@"OutputArray"] || [param isEqualToString:@"OutputArrayOfArrays"]) {
-            if ([itsType isEqualToString:@"Mat"]) {
+            if ([itsType isEqualToString:@"NSDictionary"]) {
                 NSDictionary* matMap = (NSDictionary*)[hashMap valueForKey:paramNum];
                 int matIndex = [(NSNumber*)[matMap valueForKey:@"matIndex"] intValue];
                 Mat dMat = [MatManager.sharedMgr  matAtIndex:matIndex];
@@ -116,40 +160,18 @@ void callOpenCvMethod(ArgTypes *...p) {
                 self.dstMatIndex = matIndex;
             }
         }
-        else if ([param isEqualToString:@"const std::vector<int>&"]) {
+        else if ([itsType isEqualToString:@"NSNumber"]) {
             // not sure what to do here exactly ...
-            int j = i + 1;
-            j++;
+            NSNumber *dNum = (NSNumber*)[hashMap valueForKey:paramNum];
+            [retObjs insertObject:dNum atIndex:(i-1)];
         }
-        else if ([param isEqualToString:@"const std::vector<float>&"]) {
-            // again not sure what to do here yet ...
-            int k = i + 4;
-            k+=2;
-        }
-        else if ([param isEqualToString:@"const std::vector<double>&"]) {
-            // again not sure what to do here yet ...
-            int k = i + 5;
-            k--;
-        }
-        else if ([param isEqualToString:@"bool"]) {
-            // again same fuckin thing ... fuck
-        }
-        else if ([param isEqualToString:@"int"]) {
+        else if ([param isEqualToString:@"Scalar"]) {
             
         }
-        else if ([param isEqualToString:@"float"]) {
+        else if ([param isEqualToString:@"Point"]) {
             
         }
-        else if ([param isEqualToString:@"double"]) {
-            
-        }
-        else if ([param isEqualToString:@"cv::Scalar"]) {
-            
-        }
-        else if ([param isEqualToString:@"cv::Point"]) {
-            
-        }
-        else if ([param isEqualToString:@"cv::Size"]) {
+        else if ([param isEqualToString:@"Size"]) {
             
         }
         i++;
@@ -157,34 +179,39 @@ void callOpenCvMethod(ArgTypes *...p) {
     return retObjs;
 }
 
--(Method)findMethod:(NSString*)func params:(NSDictionary*)params searchClass:(Class)searchClass {
-    Method retMethod = NULL;
+-(int)findMethod:(NSString*)func params:(NSDictionary*)params searchClass:(NSString*)searchClass {
+    int funcIndex = -1;
     int numParams = 0;
     if (params != NULL) {
         numParams = [CvInvoke getNumKeys:params];
     }
-    //SEL method = NSSelectorFromString(func);
-    //NSMethodSignature* methodParams = [searchClass methodSignatureForClass:method];
     
-    unsigned int numMethods = 0;
-    Method *methods = class_copyMethodList(searchClass, &numMethods);
-    
-    for (int i=0;i < numMethods;i++) {
-        NSString* methodName = [NSString stringWithUTF8String:sel_getName(method_getName(methods[i]))];
-        if ([methodName isEqualToString:func]) {
-            if (numParams > 0) {
-                if (numParams == method_getNumberOfArguments(methods[i])) {
-                    retMethod = methods[i];
+    int i = 0;
+    std::vector<std::string>::iterator it;  // declare an iterator to a vector of strings
+    std::vector<std::string> lookup;
+    std::vector<std::string> ptypes;
+    if ([searchClass isEqualToString:@"Imgproc"]) {
+        lookup = Imgproc;
+        ptypes = iptypes;
+    }
+    for (it=lookup.begin();it != lookup.end();it++,i++) {
+        NSString *lookupStr = [NSString stringWithUTF8String:lookup[i].c_str()];
+        if ([lookupStr isEqualToString:func]) {
+            NSString *paramTypesStr = [NSString stringWithUTF8String:ptypes[i].c_str()];
+            if ([paramTypesStr isEqualToString:@""] && numParams == 0) {
+                funcIndex = i;
+                break;
+            }
+            else {
+                NSArray *paramTypes = [paramTypesStr componentsSeparatedByString:@","];
+                if (paramTypes.count == numParams) {
+                    funcIndex = i;
                     break;
                 }
             }
-            else {
-                retMethod = methods[i];
-                break;
-            }
         }
     }
-    return retMethod;
+    return funcIndex;
 }
 
 // static method ...
@@ -266,20 +293,13 @@ void callOpenCvMethod(ArgTypes *...p) {
 }
 
 // simple helper function ...
--(NSArray*)getParameterTypes:(Method)method {
-    unsigned int numParams = method_getNumberOfArguments(method);
-    NSMutableArray *classesArr = [[NSMutableArray alloc] initWithCapacity:numParams];
-    
-    for (unsigned int i=0;i < numParams;i++) {
-        char *argumentTypeStr = (char*)calloc(2048, 1);
-        size_t strlen = 2048;
-        method_getArgumentType(method, i, argumentTypeStr, strlen);
-        NSString *argumentType = [NSString stringWithUTF8String:argumentTypeStr];
-        Class dClass = NSClassFromString(argumentType);
-        [classesArr insertObject:dClass atIndex:i];
-        free(argumentTypeStr);
+-(NSArray*)getParameterTypes:(int)methodIndex searchClass:(NSString*)searchClass {
+    std::vector<std::string> ptypes;
+    if ([searchClass isEqualToString:@"Imgproc"]) {
+        ptypes = iptypes;
     }
-    return classesArr;
+    NSString *paramTypesStr = [NSString stringWithUTF8String:ptypes[methodIndex].c_str()];
+    return [paramTypesStr componentsSeparatedByString:@","];
 }
 
 -(int)invokeCvMethod:(NSString*)in func:(NSString*)func params:(NSDictionary*)params out:(NSString*)out {
@@ -290,99 +310,69 @@ void callOpenCvMethod(ArgTypes *...p) {
        numParams = [CvInvoke getNumKeys:params];
    }
    NSArray *objects = NULL;
+   int methodIndex = -1;
+   NSString *searchClass;
    
    @try {
-       Method method = NULL;
-       
-       NSString *bundlePath = [[[NSBundle mainBundle] builtInPlugInsPath]           // 1
-                     stringByAppendingPathComponent:@"../opencv2.bundle/opencv2.framework"];
-       
-       NSURL *bundleURL = [NSURL fileURLWithPath:bundlePath];
-       CFBundleRef cfBundle = CFBundleCreate(kCFAllocatorDefault, (CFURLRef)bundleURL);
-
-       CFBundleRef ocv2 = CFBundleGetBundleWithIdentifier(CFSTR("opencv2"));
-       CFArrayRef loadedFrameworks = CFBundleGetAllBundles();
-       
-       NSArray *lframes2 = [NSBundle allBundles];
-       
-       NSArray *lframes3 = [NSBundle allFrameworks];
-       NSString *disPath = [[NSBundle mainBundle] builtInPlugInsPath];
-       
-       long discount = CFArrayGetCount(loadedFrameworks);
-       //or (int i=0;i < discount;i++) {
-         //  CFBundleRef disherebundle = (CFBundleRef)CFArrayGetValueAtIndex(loadedFrameworks, i);
-           
-           
-           NSBundle *amazingBundle = [NSBundle bundleForClass:[self class]];
-
-       NSString *ident = [amazingBundle bundleIdentifier];
-       CFBundleRef br = CFBundleGetMainBundle();
-    
-       //void* doSomething = CFBundleGetFunctionPointerForName(cfBundle,
-                                                             //CFSTR("cvtColorTwoPlane"));
-           
-           //if (doSomething != NULL) {
-             //  NSLog(@"Stop here");
-           //}
-       //}
-       NSString *fuckingmatid = @"cvtColor";
-       
-       typedef void (*opencv_t);
-       std::string yourfunc("cvtColor");
-       
        typedef std::function<void(cv::InputArray,cv::OutputArray,int,int)> fun;
        
        fun f_display = cvtColor;
        
        Scalar usethisscalar(255,255,0,255);
-       Mat inmat(500,500,CV_8UC4,usethisscalar);
-       Mat outmat;
+       ocvtypes inmat = Mat(500,500,CV_8UC4,usethisscalar);
+       ocvtypes outmat = Mat();
+       //ocvtypes inmat2 = arr1;
+       //ocvtypes outmat2 = outmat;
        
-       std::vector<void*> ps;
+       std::vector<ocvtypes*> ps;
+       ocvtypes thirdval = 6;
+       ocvtypes fourthval = 0;
+       //ocvtypes fiveval = thirdval;
+       //ocvtypes sixval = fourthval;
+       
        ps.push_back(&inmat);
-       std::vector<void*> ps2;
-       ps2.push_back(&outmat);
-       int thirdval = 6;
-       std::vector<void*> ps3;
-       ps3.push_back(&thirdval);
-       int fourthval = 0;
-       std::vector<void*> ps4;
-       ps4.push_back(&fourthval);
+       ps.push_back(&outmat);
+       ps.push_back(&thirdval);
+       ps.push_back(&fourthval);
        
-       callOpenCvMethod(&inmat, &outmat, &thirdval, &fourthval);
+       callOpenCvMethod(std::string("Imgproc"), std::string("cvtColor"), ps);
        
-       //f_display(inmat,outmat,6,0);
+       //callOpenCvMethod((Mat)&ps[0], (Mat)&ps[1], (int)&ps[2], (int)&ps[3]);
        
-       Class matclass = [fuckingmatid class]; //NSClassFromString(@"cv::Mat");
-       Class imgprocclass = NSClassFromString(@"cv::Imgproc");
-       Class coreclass = NSClassFromString(@"cv::Core");
+       //f_display(&ps[0],&ps[1],,0);
        
-       method = [self findMethod:func params:params searchClass:matclass];
-
        if (in != NULL && ![in isEqualToString:@""] && ([in isEqualToString:@"rgba"] || [in isEqualToString:@"rgbat"] || [in isEqualToString:@"gray"] || [in isEqualToString:@"grayt"] || [self.matParams.allKeys containsObject:in]))
-           {
-           
-           method = [self findMethod:func params:params searchClass:matclass];
-           }
+       {
+            methodIndex = [self findMethod:func params:params searchClass:@"Mat"];
+            if (methodIndex >= 0) {
+                searchClass = @"Mat";
+            }
+       }
        else {
-           method = [self findMethod:func params:params searchClass:imgprocclass];
-           if (method == NULL) {
-               method = [self findMethod:func params:params searchClass:coreclass];
+           methodIndex = [self findMethod:func params:params searchClass:@"Imgproc"];
+           if (methodIndex >= 0) {
+               searchClass = @"Imgproc";
+           }
+           else {
+               methodIndex = [self findMethod:func params:params searchClass:@"Core"];
+               if (methodIndex >= 0) {
+                   searchClass = @"Core";
+               }
            }
        }
        
-       if (method == NULL) {
+       if (methodIndex == -1) {
            [NSException raise:@"Method not found" format:@"%@ not found make sure method exists and is part of Opencv Imgproc, Core or Mat.", func];
        }
        if (numParams > 0) {
-           NSArray *methodParams = [self getParameterTypes:method];
+           NSArray *methodParams = [self getParameterTypes:methodIndex searchClass:searchClass];
            objects = [self getObjectArr:params params:methodParams];
            
            if (numParams != objects.count) {
                [NSException raise:@"Invalid parameter" format:@"One of the parameters is invalid and %@ cannot be invoked.", func];
            }
        }
-       if (method != NULL) {
+       if (methodIndex != -1) {
            Mat matToUse;
            
            if (in != NULL && [in isEqualToString:@"rgba"]) {
@@ -402,51 +392,16 @@ void callOpenCvMethod(ArgTypes *...p) {
                matToUse = MW2.myMat;
            }
            
-           SEL theSelector = method_getName(method);
-           NSMethodSignature *methodSignature = NULL;
-           Class dClass;
-           
-           if (out != NULL && ![out isEqualToString:@""]) {
-               methodSignature = [matclass methodSignatureForSelector:theSelector];
-               if (methodSignature != NULL) {
-                   dClass = matclass;
-               }
-           }
-           else {
-               methodSignature = [imgprocclass methodSignatureForSelector:theSelector];
-               if (methodSignature == NULL) {
-                   methodSignature = [coreclass methodSignatureForSelector:theSelector];
-                   if (methodSignature != NULL) {
-                       dClass = coreclass;
-                   }
-               }
-               else {
-                   dClass = imgprocclass;
-               }
-           }
-           
            if (matToUse.rows > 0 && [func isEqualToString:@"release"]) {
                // special case deleting the last Mat
                matToUse.release();
                [self.matParams removeObjectForKey:in];
            }
-           else if (methodSignature != NULL) {
-               NSInvocation *invoker = [NSInvocation invocationWithMethodSignature:methodSignature];
-               [invoker setSelector:theSelector];
-               [invoker setTarget:dClass];
-               
-               for (int i=0;i < objects.count;i++) {
-                   id parm = [objects objectAtIndex:i];
-                   [invoker setArgument:&parm atIndex:i];
-               }
-               [invoker invoke];
+           else {
                id matParam;
-               [invoker getReturnValue:&matParam];
                
                if (matParam != NULL) {
-                   if ([matParam isKindOfClass:matclass]) {
-                       [self.matParams setObject:matParam forKey:out];
-                   }
+                   [self.matParams setObject:matParam forKey:out];
                }
            }
        }
